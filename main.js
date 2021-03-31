4973afcbb544a0f4837f0337e803a8202aff868b
Marius <healtybo@gmail.com>
2021-03-31 16:38:15 +0300

    Add button to clear grid

--- main.js
+++ main.js
@@ -2,20 +2,32 @@
 
 let container = document.querySelector(".container");
 
-window.addEventListener('load', (event) => {
+window.addEventListener('load', () => {
     let htmlElements = 256;
 
     for (let i = 0; i < htmlElements; i++) {
         let containers = document.createElement("div");
         containers.className = "containers";
         container.appendChild(containers);
-    }
+    } 
 });
 
 //Mouseover event listener and function which colors div
 
 container.addEventListener('mouseover', e => {
+    if (e.target.classList.contains('containers')) {
 e.target.style.backgroundColor = "black";
+    }
 });
 
+//Button to clear grid
+
+let clearBtn = document.querySelector('.clearBtn');
+let divs = document.querySelectorAll('.containers');
+
+clearBtn.addEventListener('click', () => {
+    for (i = 0; i < 256; i++) {
+container.children[i].setAttribute("style", "background-colo: black");
+    }
+});
 
