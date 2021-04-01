dbc69e99e4714da365802ec9f7d9c886a9eeb82d
Marius <healtybo@gmail.com>
2021-04-01 17:49:23 +0300

    Add function to change grid size and UI improvements

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
-//Add event listener which fires once window is loaded and creates 16 x 16 grid
+
+//Mouseover event listener and function which colors div in black
 
 let container = document.querySelector(".container");
 
-window.addEventListener('load', () => {
-    let htmlElements = 256;
-
-    for (let i = 0; i < htmlElements; i++) {
-        let containers = document.createElement("div");
-        containers.className = "containers";
-        container.appendChild(containers);
-    }
-});
-
-//Mouseover event listener and function which colors div in black
 let blackBtn = document.querySelector('.blackBtn');
 
 blackBtn.addEventListener('click', (e) => {
@@ -25,50 +15,60 @@ blackBtn.addEventListener('click', (e) => {
     })
 });
 
-//Button to clear grid
-
-let clearBtn = document.querySelector('.clearBtn');
-let divs = document.querySelectorAll('.containers');
-
-clearBtn.addEventListener('click', () => {
-    for (i = 0; i < 4096; i++) {
-        container.children[i].setAttribute("style", "background-colo: black");
-    }
-});
-
 //Function to generate random RGB based color
 
 let colorBtn = document.querySelector('.colorBtn');
 
-colorBtn.addEventListener('click', (e) => {
+colorBtn.addEventListener('click', () => {
 
     container.addEventListener('mouseover', e => {
 
         var randomColor = Math.floor(Math.random() * 16777215).toString(16);
         if (e.target.classList.contains('containers')) {
-        e.target.style.backgroundColor = "#" + randomColor;
+            e.target.style.backgroundColor = "#" + randomColor;
         }
     })
 });
 
 //Function to change grid size
 
-let changeInput = document.getElementById('changeSize');
-
-changeInput.addEventListener('keyup', () => {
-    if (changeInput.value >= 1 && changeInput.value <= 64) {
-
-        let htmlElements = changeInput.value * changeInput.value;
-        for (let i = 0; i < htmlElements; i++) {
-            let containers = document.createElement("div");
-            containers.className = "containers";
-            container.appendChild(containers);
-        }
-    } else {
-        confirm('Enter number between 1-64');
-        }
-
+$(document).ready(function () {
+    grid(16, 16);
 });
 
+$(".button").click(function () {
+    var size = prompt("Please select your grid size from 1-64");
+    grid(size, size);
+});
+
+function grid(rows, cols) {
+    var table = "<table>";
+    var size = (1 / rows * 525) + "px";
+
+    for (i = 0; i < rows; i++) {
+        table += "<tr>";
+        for (j = 0; j < cols; j++) {
+            table += "<td>" + "</td>";
+        }
+        table += "</tr>";
+    }
+    table += "</table>";
+
+    $(".container").empty().append(table);
+    $("tr").css("height", size);
+    $("td").css("color", "black").css("width", size);
+    $("td").addClass("containers");
+};
+
+//Button to clear grid
+
+let clearBtn = document.querySelector('.clearBtn');
+let tds = document.getElementsByTagName('td');
+
+clearBtn.addEventListener('click', () => {
+    for (i = 0; i < tds.length; ++i) {
+        tds[i].style.backgroundColor = "transparent";
+    }
+});
 
 
